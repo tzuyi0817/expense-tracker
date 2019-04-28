9825d130b329bdb74b140e12ca87545deb8746d3
Tzuyi Yang <a0988426059@gmail.com>
2019-04-28 19:42:40 +0800

    Setting mongodb

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 //setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,6 +13,21 @@ app.set('view engine', 'handlebars')
 
 //setting css
 app.use(express.static('public'))
+
+//setting mongodb
+mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useCreateIndex: true })
+
+const db = mongoose.connection
+
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
 //setting routers
