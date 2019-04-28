2bba38d0c2ed6e3e800b9d655c0869fd11a26726
Tzuyi Yang <a0988426059@gmail.com>
2019-04-28 19:08:38 +0800

    Add main and index page

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+//setting handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting css
+app.use(express.static('public'))
 
 
 //setting routers
