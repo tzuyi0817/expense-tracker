757d795449d72ddafdcaebf2630e579f55c960f1
Tzuyi Yang <a0988426059@gmail.com>
2019-04-29 23:46:49 +0800

    Add home template

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../models/record')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find((err, records) => {
+    let category = records.category
+    if (category === '餐飲食品') {
+      category = 'fas fa-utensils'
+    }
+    if (category === '交通出行') {
+      category = 'fas fa-shuttle-van'
+    }
+    if (category === '休閒娛樂') {
+      category = 'fas fa-grin-beam'
+    }
+    if (category === '家居物業') {
+      category = 'fas fa-home'
+    }
+    if (category === '其他') {
+      category = 'fas fa-pen'
+    }
+    res.render('index', { records })
+  })
 })
 
 module.exports = router
