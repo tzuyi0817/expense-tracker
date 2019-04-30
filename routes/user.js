d3b256771efbe2512594ed2afd61cf7685dde518
Tzuyi Yang <a0988426059@gmail.com>
2019-04-30 16:54:07 +0800

    Add bcryptjs

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 //登入頁面
 router.get('/login', (req, res) => {
@@ -31,10 +32,19 @@ router.post('/register', (req, res) => {
       res.render('register', { name, email, password, password2 })
     } else {
       const newUser = new User({ name, email, password })
-      newUser.save().then(user => {
-        res.redirect('/')
+
+      //bcrypt
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+
+          newUser.save().then(user => {
+            res.redirect('/')
+          })
+            .catch(err => console.log(err))
+        })
       })
-        .catch(err => console.log(err))
     }
   })
 })
