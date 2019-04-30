e30dc270e50ea1d68cc1c9b4c98f08bb4fc26e68
Tzuyi Yang <a0988426059@gmail.com>
2019-04-30 15:02:26 +0800

    Add user route and model

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+//登入頁面
+router.get('/login', (req, res) => {
+
+})
+
+//登入撿查
+router.post('/login', (req, res) => {
+
+})
+
+//註冊頁面
+router.get('/register', (req, res) => {
+
+})
+
+//註冊撿查
+router.post('/register', (req, res) => {
+
+})
+
+//登出
+router.post('/logout', (req, res) => {
+
+})
