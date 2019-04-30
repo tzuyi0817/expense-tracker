075836847c3e146def0a5638727317f8cfae49e0
Tzuyi Yang <a0988426059@gmail.com>
2019-04-30 10:24:46 +0800

    Add edit features

--- routes/record.js
+++ routes/record.js
@@ -23,12 +23,40 @@ router.post('/new', (req, res) => {
 
 //編輯頁面
 router.get('/:id/edit', (req, res) => {
-  res.render('edit')
+  Record.findById(req.params.id, (err, record) => {
+    if (err) return console.log(err)
+
+    let optionASelected = false
+    let optionBSelected = false
+    let optionCSelected = false
+    let optionDSelected = false
+    let optionESelected = false
+    if (record.category === 'fas fa-home') {
+      optionASelected = true
+    } else if (record.category === 'fas fa-shuttle-van') {
+      optionBSelected = true
+    } else if (record.category === 'fas fa-grin-beam') {
+      optionCSelected = true
+    } else if (record.category === 'fas fa-utensils') {
+      optionDSelected = true
+    } else if (record.category === 'fas fa-pen') {
+      optionESelected = true
+    }
+    res.render('edit', { record, optionASelected, optionBSelected, optionCSelected, optionDSelected, optionESelected })
+  })
 })
+
 
 //編輯資料
 router.post('/:id', (req, res) => {
+  Record.findById(req.params.id, (err, record) => {
+    if (err) return console.log(err)
+    Object.assign(record, req.body)
 
+    record.save(err => {
+      res.redirect(`/`)
+    })
+  })
 })
 
 //刪除資料
